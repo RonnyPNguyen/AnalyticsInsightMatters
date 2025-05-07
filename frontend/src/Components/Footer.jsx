21ad67fb9f6eb557a7cfb7101d681bd4797b4165
Ronny P Nguyen <phuong.nguyentien192@gmail.com>
2025-05-07 21:04:44 +1000

    daily commit

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div>
+			<div className="Author">
+				<p>Author: John Doe</p>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ frontend/src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => {
+	return (
+		<div>
+			<div className="logo"></div>
+			<div className="Pages">
+				<ul>
+					<li>Home Page</li>
+					<li>Listing</li>
+					<li>About</li>
+				</ul>
+			</div>
+			<div>Login</div>
+		</div>
+	);
+};
+
+export default Nav;
