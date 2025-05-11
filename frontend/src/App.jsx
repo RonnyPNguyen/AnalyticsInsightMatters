2a76b9af82ed4976ca941e58fca7847e9887891e
Ronny P Nguyen <phuong.nguyentien192@gmail.com>
2025-05-12 00:39:58 +1000

    May 12 12.39am

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,27 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+// Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Card from "./components/Card";
-import About from "./pages/About";
-import Featured from "./components/Featured";
+import BizListings from "./components/BizListings";
+import ViewAllOffer from "./components/ViewAllOffer";
 
+// Pages
+import About from "./pages/About";
 import Home from "./pages/Home";
 import Listings from "./pages/Listings";
 import Login from "./pages/Login";
 
 function App() {
-	let selectedPage = <Listings />;
 	return (
 		<>
-			<Navbar></Navbar>
-			<Hero></Hero>
 			<h1> This is the Main APP</h1>
-			<div>{selectedPage}</div>
-			<div>
-				<h2 className="text-indigo text-center text-3xl">Feature Business</h2>
-				<div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-					<Featured></Featured>
-				</div>
-			</div>
-			<Footer></Footer>
+			<Navbar />
+			<Hero />
+			<BizListings />
+			<ViewAllOffer />
+			<Footer />
 		</>
 	);
 }
---
+++ frontend/src/Components/ViewAllOffer.jsx
@@ -0,0 +1,13 @@
+const ViewAllOffer = () => {
+	return (
+		<section className="m-auto max-w-lg text-center py-10">
+			<a
+				href="/Listings"
+				className="py-4 px-6 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300"
+			>
+				View All Offers
+			</a>
+		</section>
+	);
+};
+export default ViewAllOffer;
